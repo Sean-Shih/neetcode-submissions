4b86ee01a5ef477ccd24393df4939622de3980e1
Syuan Kai (Sean) Shih <sean.shih.2005@gmail.com>
2026-05-07 15:49:45 -0700

    Add: singlyLinkedList - submission-1

---
+++ Data Structures & Algorithms/singlyLinkedList/submission-1.py
@@ -0,0 +1,53 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+class LinkedList:
+    
+    def __init__(self):
+        self.head = Node(-1)
+        self.tail = self.head
+    
+    def get(self, index: int) -> int:
+        cur = self.head.next
+        i = 0
+        while cur:
+            if i == index:
+                return cur.val
+            i += 1
+            cur = cur.next
+        return -1
+
+    def insertHead(self, val: int) -> None:
+        new_node = Node(val)
+        new_node.next = self.head.next
+        self.head.next = new_node
+        if not new_node.next:
+            self.tail = new_node
+
+    def insertTail(self, val: int) -> None:
+        self.tail.next = Node(val)
+        self.tail = self.tail.next
+
+    def remove(self, index: int) -> bool:
+        i = 0
+        cur = self.head
+        while i < index and cur:
+            i += 1
+            cur = cur.next
+        
+        if cur and cur.next:
+            if cur.next == self.tail:
+                self.tail = cur
+            cur.next = cur.next.next
+            return True
+        return False
+
+    def getValues(self) -> List[int]:
+        cur = self.head.next
+        res = []
+        while cur:
+            res.append(cur.val)
+            cur = cur.next
+        return res
